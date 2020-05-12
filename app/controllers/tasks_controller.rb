0feea805ce612303032f70af787ede00fbfb2d5d
Ilayda Bakare <ilayda.bakare@gmail.com>
2020-05-12 13:06:42 +0100

    up to step 4 in challenge, need to add links back to index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all # Remember, class methods have to be SINGULAR!
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
 end
+
+# make new html pages for index not for /tasks because they need to match up
+# with methods in the controller
